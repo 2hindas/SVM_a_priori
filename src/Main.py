1fc815ab913873553424dd63c631f79d461f476b
Tuhin Das <t.das@student.tudelft.nl>
2020-05-10 18:28:37 +0200

    Setup MNIST

--- src/Main.py
+++ src/Main.py
@@ -14,92 +14,6 @@ from src.Kernels import JitteredKernel
 pd.set_option('display.width', 320)
 np.set_printoptions(linewidth=320)
 
-px = 20
-num_f = px ** 2
-
-
-def jitter_kernel(x_matrix, y_matrix):
-    var = x_matrix.var()
-
-    def f(a, b, v):
-        gamma = 1 / (num_f * v)
-        return rbf_kernel(a, b, gamma)
-
-    x_length = x_matrix.shape[0]
-
-    max_matrix = f(x_matrix, y_matrix, var)
-
-    transformations = [(1, 0),  # D
-                       (-1, 0),  # U
-                       (0, 1),  # R
-                       (0, -1),  # L
-                       (1, 1),  # RD
-                       (1, -1),  # LD
-                       (-1, 1),  # RU
-                       (-1, -1)]  # LU
-
-    x_reshaped = x_matrix.reshape((x_length, px, px))
-
-    # Translation
-    for t in transformations[2:4]:
-        for i in range(1, 3):
-            trans = im.shift(x_reshaped, (0, t[0] * i, t[1] * i), mode='constant', cval=-1)
-            max_matrix = np.maximum(max_matrix, f(trans.reshape((x_length, 400)), y_matrix, var))
-
-    # Rotation
-    # for i in range(0, 3):
-    #     if i != 1:
-    #         angle = -4.0 + i * 4.0
-    #         trans = im.rotate(x_reshaped, axes=(1, 2), order=1, angle=angle, mode='constant', cval=-1,
-    #                                    reshape=False)
-    #         max_matrix = np.maximum(max_matrix, f(trans.reshape((x_length, num_f)), y_matrix, var))
-
-    # Scaling
-    # trans = im.zoom(x_reshaped, (1.0, 1.1, 1.1), mode='constant', cval=-1, order=1)[:, 1:-1, 1:-1]
-    # max_matrix = np.maximum(max_matrix, f(trans.reshape((x_length, num_f)), y_matrix, var))
-
-    return max_matrix
-
-
-def jitter_kernel2(x_matrix, y_matrix):
-    var = x_matrix.var()
-
-    def f(a, b, v):
-        gamma = 1 / (num_f * v)
-        return rbf_kernel(a, b, gamma)
-
-    x_length = x_matrix.shape[0]
-
-    max_matrix = f(x_matrix, y_matrix, var)
-
-    transformations = [(1, 0),  # D
-                       (-1, 0),  # U
-                       (0, 1),  # R
-                       (0, -1),  # L
-                       (1, 1),  # RD
-                       (1, -1),  # LD
-                       (-1, 1),  # RU
-                       (-1, -1)]  # LU
-
-    x_reshaped = x_matrix.reshape((x_length, px, px))
-
-    for t in transformations[4:8]:
-        for i in range(1, 3):
-            trans = im.shift(x_reshaped, (0, t[0] * i, t[1] * i), mode='constant', cval=-1)
-            max_matrix = np.maximum(max_matrix, f(trans.reshape((x_length, 400)), y_matrix, var))
-
-    # for i in range(0, 3):
-    #     if i != 1:
-    #         angle = -4.0 + i * 4.0
-    #         transformation = im.rotate(x_reshaped, axes=(1, 2), order=1, angle=angle, mode='constant', cval=-1,
-    #                                    reshape=False)
-    #         max_matrix = np.maximum(max_matrix, f(transformation.reshape((x_length, num_f)), y_matrix, var))
-
-    return max_matrix
-
-
-train = pd.read_csv("../data/USPS_train.csv", delim_whitespace=True)
-test = pd.read_csv("../data/USPS_test.csv", delim_whitespace=True)
 
 transformations = [(1, 0),  # D
                    (-1, 0),  # U
@@ -110,31 +24,23 @@ transformations = [(1, 0),  # D
                    (-1, 1),  # RU
                    (-1, -1)]  # LU
 
-train_features = train.iloc[:, 1:].to_numpy()
-train_targets = train.iloc[:, 0].to_numpy()
-test_features = test.iloc[:, 1:].to_numpy()
-test_targets = test.iloc[:, 0].to_numpy()
-
+features = pd.read_csv("../data/mnist_train_features.csv")
+targets = pd.read_csv("../data/mnist_train_targets.csv").values.flatten()
+test_features = pd.read_csv("../data/mnist_test_features.csv")
+test_targets = pd.read_csv("../data/mnist_test_targets.csv").values.flatten()
 print("Data has been read")
-
-train_features = np.pad(train_features.reshape((7290, 16, 16)), ((0, 0), (2, 2), (2, 2)), 'constant',
-                        constant_values=(-1)).reshape((7290, num_f))
-test_features = np.pad(test_features.reshape((2006, 16, 16)), ((0, 0), (2, 2), (2, 2)), 'constant',
-                       constant_values=(-1)).reshape((2006, num_f))
-
-model = svm.SVC(kernel=jitter_kernel, cache_size=900)
 
 
 print("Training...")
 
-svm = SupportVectorMachine(train_features, train_targets, test_features, test_targets)
+svm = SupportVectorMachine(features[0:1000], targets[0:1000], test_features, test_targets)
 
 start = timer()
 
 # svm.set_kernel(JitteredKernel((transformations[0:4], 1, 2)))
 # svm.train()
 # print("Error: " + str(svm.error()))
-svm.set_kernel(JitteredKernel((transformations[0:8], 1, 1), (-5, 5, 5)))
+# svm.set_kernel(JitteredKernel((transformations[0:8], 1, 1), (-5, 5, 5)))
 # svm.train()
 # print("Error: " + str(svm.error()))
 # svm.
@@ -144,9 +50,14 @@ print("Time: " + str(end-start))
 print("Error: " + str(svm.error()))
 exit(0)
 
+
+
+
+
+
+
 """
-VSV 19.507 seconds
-Virtual Features 36.627 seconds
+USPS
 
 91.719 seconds error 3.84
 
