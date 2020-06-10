99f9b37116f1d9c2df1d4e94276e364b6241d9f0
Tuhin Das <t.das@student.tudelft.nl>
2020-06-10 21:08:14 +0200

    remove files

--- src/EnsembleSVM.py
+++ src/EnsembleSVM.py
@@ -7,26 +7,28 @@ from scipy.stats import mode
 
 class EnsembleSVM:
 
-    def __init__(self, train_features, train_targets, test_features, test_targets, constant):
+    def __init__(self, train_features, train_targets, test_features, test_targets, constant, support_vectors=None, support_targets=None):
         self.features = train_features
         self.targets = train_targets
         self.test_features = test_features
         self.test_targets = test_targets
-        self.num_features = self.features.shape[1]
+        self.num_features = self.test_features.shape[1]
         self.sqrt_features = int(np.sqrt(self.num_features))
         self.models = []
         self.C = constant
-        self.original_model = svm.SVC(kernel='rbf', cache_size=1000, C=constant)
-
-        self.original_model.fit(train_features, train_targets)
-
-        print(accuracy_score(test_targets, self.original_model.predict(test_features)))
-
-        self.support_vectors = train_features[self.original_model.support_]
-        self.support_vector_targets = train_targets[self.original_model.support_]
+        if support_vectors is None:
+            self.original_model = svm.SVC(kernel='rbf', cache_size=1000, C=constant)
+            self.original_model.fit(train_features, train_targets)
+            print(accuracy_score(test_targets, self.original_model.predict(test_features)))
+            self.support_vectors = train_features[self.original_model.support_]
+            self.support_vector_targets = train_targets[self.original_model.support_]
+        else:
+            self.support_vectors = support_vectors
+            self.support_vector_targets = support_targets
         self.feat = self.support_vectors
         self.targ = self.support_vector_targets
         print(f"Support Vectors: {len(self.feat)}")
+
 
     def train(self, replace=False):
         model = svm.SVC(kernel='rbf', cache_size=1000, C=self.C)
@@ -39,30 +41,33 @@ class EnsembleSVM:
         self.feat = self.support_vectors
         self.targ = self.support_vector_targets
 
+
     def add_translations(self, directions, min_trans, max_trans):
-        for d in directions:
-            for i in range(min_trans, max_trans + 1):
-                num_sv = len(self.support_vectors)
-                transformation = im.shift(
-                    self.support_vectors.reshape(
-                        (num_sv, self.sqrt_features, self.sqrt_features)),
-                    (0, d[0] * i, d[1] * i), mode='constant', cval=-1)
-                translated_features = transformation.reshape((num_sv, self.num_features))
-                self.targ = np.append(self.targ, self.support_vector_targets)
-                self.feat = np.vstack((translated_features, self.feat))
+          for d in directions:
+              for i in range(min_trans, max_trans + 1):
+                  num_sv = len(self.support_vectors)
+                  transformation = im.shift(
+                      self.support_vectors.reshape(
+                          (num_sv, self.sqrt_features, self.sqrt_features)),
+                      (0, d[0] * i, d[1] * i), mode='constant', cval=-1)
+                  translated_features = transformation.reshape((num_sv, self.num_features))
+                  self.targ = np.append(self.targ, self.support_vector_targets)
+                  self.feat = np.vstack((translated_features, self.feat))
+
 
     def add_rotation(self, min_degrees, max_degrees, step_size):
-        for angle in range(min_degrees, max_degrees + 1, step_size):
-            if angle == 0:
-                continue
-            num_sv = len(self.support_vectors)
-            transformation = im.rotate(
-                self.support_vectors.reshape((num_sv, self.sqrt_features, self.sqrt_features)),
-                axes=(1, 2), order=1, angle=angle,
-                mode='constant', cval=-1, reshape=False)
-            translated_features = transformation.reshape((num_sv, self.num_features))
-            self.targ = np.append(self.targ, self.support_vector_targets)
-            self.feat = np.vstack((translated_features, self.feat))
+          for angle in range(min_degrees, max_degrees + 1, step_size):
+              if angle == 0:
+                  continue
+              num_sv = len(self.support_vectors)
+              transformation = im.rotate(
+                  self.support_vectors.reshape((num_sv, self.sqrt_features, self.sqrt_features)),
+                  axes=(1, 2), order=1, angle=angle,
+                  mode='constant', cval=-1, reshape=False)
+              translated_features = transformation.reshape((num_sv, self.num_features))
+              self.targ = np.append(self.targ, self.support_vector_targets)
+              self.feat = np.vstack((translated_features, self.feat))
+
 
     def predict(self, X):
 
@@ -120,3 +125,20 @@ class EnsembleSVM:
             # return np.round(100 - 100 * accuracy_score(self.test_targets, self.predict(self.test_features)), 2)
         else:
             return np.round(100 - 100 * accuracy_score(self.test_targets, self.predict(X)), 2)
+
+    def train_pasting(self, sample_ratio, num_machines):
+        size = len(self.feat)
+        print(f"Training set size: {size}")
+        for _ in range(num_machines):
+            model = svm.SVC(kernel='rbf', cache_size=1000, C=self.C)
+            while True:
+                sample_size = int(sample_ratio * size)
+                sample = np.random.choice(size, sample_size, replace=False)
+                sample_feat = self.feat[sample]
+                sample_targ = self.targ[sample]
+                if np.unique(sample_targ).shape[0] == 10:
+                    break
+            model.fit(sample_feat, sample_targ)
+            self.models.append(model)
+        self.feat = self.support_vectors
+        self.targ = self.support_vector_targets
--- src/Main.py
+++ src/Main.py
@@ -3,7 +3,7 @@ from timeit import default_timer as timer
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
-
+import hickle as hkl
 
 from src.EnsembleSVM import EnsembleSVM
 
@@ -12,24 +12,19 @@ scaler = MinMaxScaler((-1, 1))
 pd.set_option('display.width', 320)
 np.set_printoptions(linewidth=320)
 
+dataset = "USPS"
+C = 10
 
+support_vectors = hkl.load(f'../data/{dataset}_{str(C)}_SV_features.hkl')
+support_vector_labels = hkl.load(f'../data/{dataset}_{str(C)}_SV_labels.hkl')
 
+train_features = hkl.load(f'../data/{dataset}_train_features.hkl')
+train_labels = hkl.load(f'../data/{dataset}_train_labels.hkl')
+test_features = hkl.load(f'../data/{dataset}_test_features.hkl')
+test_labels = hkl.load(f'../data/{dataset}_test_labels.hkl')
 
+print("Data set loaded in memory.")
 
-# num_f = 400
-# train = pd.read_csv("/content/USPS_train.csv", delim_whitespace=True)
-# test = pd.read_csv("/content/USPS_test.csv", delim_whitespace=True)
-
-# train_features = train.iloc[:, 1:].to_numpy()
-# train_targets = train.iloc[:, 0].to_numpy()
-# test_features = test.iloc[:, 1:].to_numpy()
-# test_targets = test.iloc[:, 0].to_numpy()
-
-# train_features = np.pad(train_features.reshape((7290, 16, 16)), ((0, 0), (2, 2), (2, 2)),
-#                         'constant',
-#                         constant_values=(-1)).reshape((7290, num_f))
-# test_features = np.pad(test_features.reshape((2006, 16, 16)), ((0, 0), (2, 2), (2, 2)), 'constant',
-#                        constant_values=(-1)).reshape((2006, num_f))
 
 directions = [(1, 0),  # D
               (-1, 0),  # U
@@ -42,44 +37,19 @@ directions = [(1, 0),  # D
 
 
 start = timer()
-# 5.28 is basic error C = 1
-# 1.62 is basic error C = 10
 
 print("Ensemble training, C = 1")
-ensemble = EnsembleSVM(train_features, train_targets, test_features, test_targets, 1)
+ensemble = EnsembleSVM(None, None, test_features, test_labels, 10, support_vectors, support_vector_labels)
 ensemble.add_rotation(-5, 5, 5)
-ensemble.train(True)
 ensemble.add_translations(directions[0:2], 1, 1)  # UD
-ensemble.train()
 ensemble.add_translations(directions[3:4], 1, 1)  # L
 ensemble.add_translations(directions[5:6], 1, 1)  # LD
 ensemble.add_translations(directions[7:8], 1, 1)  # LU
-ensemble.train()
 ensemble.add_translations(directions[2:3], 1, 1)  # R
 ensemble.add_translations(directions[4:5], 1, 1)  # RD
 ensemble.add_translations(directions[6:7], 1, 1)  # RU
-ensemble.train()
+ensemble.train_pasting(0.3, 5)
 
-end = timer()
-
-print(f"Error: {ensemble.error()}")
-print(f"Duration: {end - start} seconds")
-
-start = timer()
-print("Ensemble training, C = 10")
-ensemble = EnsembleSVM(train_features, train_targets, test_features, test_targets, 10)
-ensemble.add_rotation(-5, 5, 5)
-ensemble.train(True)
-ensemble.add_translations(directions[0:2], 1, 1)  # UD
-ensemble.train()
-ensemble.add_translations(directions[3:4], 1, 1)  # L
-ensemble.add_translations(directions[5:6], 1, 1)  # LD
-ensemble.add_translations(directions[7:8], 1, 1)  # LU
-ensemble.train()
-ensemble.add_translations(directions[2:3], 1, 1)  # R
-ensemble.add_translations(directions[4:5], 1, 1)  # RD
-ensemble.add_translations(directions[6:7], 1, 1)  # RU
-ensemble.train()
 end = timer()
 
 print(f"Error: {ensemble.error()}")
