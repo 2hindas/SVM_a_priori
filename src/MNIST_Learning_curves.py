9926e41d8ea04f2a384ae1b77664f10cfbbb71b2
Tuhin Das <t.das@student.tudelft.nl>
2021-03-29 18:13:51 +0200

    MNIST Learning Curve

--- src/MNIST_Learning_curves.py
+++ src/MNIST_Learning_curves.py
@@ -34,8 +34,8 @@ directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -
 gamma = 0
 
 dataset = "MNIST"
-X_train = hkl.load(f'../data/{dataset}/{dataset}_train_features.hkl')
-Y_train = hkl.load(f'../data/{dataset}/{dataset}_train_labels.hkl')
+MNIST_X_train = hkl.load(f'../data/{dataset}/{dataset}_train_features.hkl')
+MNIST_Y_train = hkl.load(f'../data/{dataset}/{dataset}_train_labels.hkl')
 X_test = hkl.load(f'../data/{dataset}/{dataset}_test_features.hkl')
 Y_test = hkl.load(f'../data/{dataset}/{dataset}_test_labels.hkl')
 
@@ -76,152 +76,47 @@ def pooling_kernel(x_matrix, y_matrix):
     return gram_matrix
 
 
-# In[25]:
-
-
 def test_error(model, features, labels):
     error = (1.0 - accuracy_score(labels, model.predict(features))) * 100
     return np.around(error, decimals=2)
 
 
-# ### Run SVM with pooling kernel
-
-# In[46]:
-
-
-gammas = np.logspace(-1.8, -1, 15)
-# gammas = np.logspace(-1.9, -0.65, 15)
-# gammas = np.logspace(-3.5, -0.65, 15)
-num_runs = 1
-
-errors = np.zeros(len(gammas))
-VSV_errors = np.zeros(len(gammas))
-
-for rstate in range(0, num_runs):
-    # Test the pooling kernel 
-
-    current_errors = []
-    for g in gammas:
-        A_gamma = B_gamma = C_gamma = g
-        model = svm.SVC(C=2, cache_size=50, kernel=pooling_kernel)
-        model.fit(X_train, Y_train)
-        current_errors.append(test_error(model, X_test, Y_test))
-    errors = np.asarray(current_errors)
-
-    # Test VSV method
-#     current_errors = []
-#     for g in gammas:
-#         model = SupportVectorMachine(X_train, Y_train, X_test, Y_test, g)
-#         model.train()
-#         model.translate_SV(directions[0:8], 1, 1)
-#         model.train()
-#         current_errors.append(model.error())
-
-#     VSV_errors += np.asarray(current_errors)
-
-# VSV_errors = VSV_errors / num_runs
-errors = errors / num_runs
-
-# In[47]:
-
-
-plt.clf()
-plt.ylim(0, 100)
-plt.plot(gammas, errors, label='Standard RBF')
-# plt.plot(gammas, errors[1], label='Pooling 2x2')
-# plt.plot(gammas, errors[2], label='Pooling 3x3')
-# plt.plot(gammas, VSV_errors, label='VSV')
-
-# plt.scatter(gammas[np.argmin(errors[0])], errors[0][np.argmin(errors[0])])
-# plt.scatter(gammas[np.argmin(errors[1])], errors[1][np.argmin(errors[1])])
-# plt.scatter(gammas[np.argmin(errors[2])], errors[2][np.argmin(errors[2])])
-# plt.scatter(gammas[np.argmin(VSV_errors)], VSV_errors[np.argmin(VSV_errors)])
-
-# plt.title(f"Artificial Lines dataset")
-plt.legend(loc='upper right')
-plt.xlabel("$\gamma$")
-plt.ylabel("Generalization error")
-print(gammas)
-print(errors)
-# print(VSV_errors)
-plt.savefig("kernel-comparison.png", dpi=400)
-# plt.show()
-
-# In[48]:
-
-
-for i in range(0, len(errors)):
-    print(f"({gammas[i]}, {errors[i]})")
-
-# print()
-
-# for i in range(0, len(VSV_errors)):
-#         print(f"({gammas[i]}, {VSV_errors[i]})")  
-
-
-# In[49]:
-
-
-print("Minimum errors:\n")
-
-# VSV_gamma = gammas[np.argmin(VSV_errors)]
-standard_gamma = gammas[np.argmin(errors)]
-# pooling2_gamma = gammas[np.argmin(errors[1])]
-# pooling3_gamma = gammas[np.argmin(errors[2])]
-
-# optimal_gammas = [standard_gamma, pooling2_gamma, pooling3_gamma, VSV_gamma]
-
-# print(f"VSV SVM: {VSV_errors[np.argmin(VSV_errors)]} with gamma {VSV_gamma}" )
-print(f"Standard SVM: {errors[np.argmin(errors[0])]} with gamma {standard_gamma}")
-# print(f"Pooling 2x2 SVM: {errors[1][np.argmin(errors[1])]} with gamma {pooling2_gamma}" )
-# print(f"Pooling 3x3 SVM: {errors[2][np.argmin(errors[2])]} with gamma {pooling3_gamma}" )
-
-exit()
-
-# ### Plot learning curves
-
-# In[150]:
-
-
-index = np.argmin(errors)
-A_gamma = B_gamma = C_gamma = gammas[index % len(gammas)]
 A_gamma = B_gamma = C_gamma = 0.005
 
 samples = range(5000, 60001, 5000)
 num_runs = 1
 
-LC_errors = np.zeros((len(basic_combinations), len(samples)))
+LC_errors = np.zeros(len(samples))
 VSV_LC_errors = np.zeros(len(samples))
 
 for rstate in range(0, num_runs):
 
     for i, num in enumerate(samples):
 
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=3 * n - num,
-                                                            random_state=rstate)
-        # Test the pooling kernel
-        for j, f in enumerate(basic_combinations):
-            A_gamma = B_gamma = C_gamma = optimal_gammas[j]
-            combiner = f
-            model = svm.SVC(C=2, cache_size=50, kernel=pooling_kernel)
-            model.fit(X_train, Y_train)
-            LC_errors[j][i] += test_error(model, X_test, Y_test)
+        indices = np.random.choice(60000, num, replace=False)
+
+        X_train = MNIST_X_train[indices]
+        Y_train = MNIST_Y_train[indices]
+
+        model = svm.SVC(C=2, cache_size=50, kernel=pooling_kernel)
+        model.fit(X_train, Y_train)
+        LC_errors[i] += test_error(model, X_test, Y_test)
 
         # Test VSV method
-        model = SupportVectorMachine(X_train, Y_train, X_test, Y_test, VSV_gamma)
-        model.train()
-        model.translate_SV(directions[0:8], 1, 1)
-        model.train()
-        VSV_LC_errors[i] += model.error()
+        # model = SupportVectorMachine(X_train, Y_train, X_test, Y_test, VSV_gamma)
+        # model.train()
+        # model.translate_SV(directions[0:8], 1, 1)
+        # model.train()
+        # VSV_LC_errors[i] += model.error()
 
 LC_errors = LC_errors / num_runs
-VSV_LC_errors = VSV_LC_errors / num_runs
+# VSV_LC_errors = VSV_LC_errors / num_runs
 
 # In[185]:
 
 
 print(LC_errors)
-print(VSV_LC_errors)
+# print(VSV_LC_errors)
 
 # plt.clf()
 # plt.ylim(0, 75)
@@ -236,19 +131,18 @@ print(VSV_LC_errors)
 # plt.savefig("learning-curve.png", dpi=400)
 # plt.show()
 
-for e in LC_errors:
-    for i in range(0, len(e)):
-        print(f"({list(range(5, 1000, 50))[i]}, {e[i]})")
+for i, e in enumerate(LC_errors):
+    print(f"({list(samples)[i]}, {e})")
 
-    print()
+print()
 
-for i in range(0, len(VSV_LC_errors)):
-    print(f"({list(range(5, 1000, 50))[i]}, {VSV_LC_errors[i]})")
+# for i in range(0, len(VSV_LC_errors)):
+#     print(f"({list(samples)[i]}, {VSV_LC_errors[i]})")
 
 # ### Performance of SVMs on noisy data
 
 # In[167]:
-
+exit()
 
 gammas = np.logspace(-1.6, -0.65, 15)
 num_runs = 5
